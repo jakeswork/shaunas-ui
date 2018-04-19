402727d76aad95a6ee9d00a1d22d4c882b406b98
Jake <jflynn@reapit.com>
2018-04-19 17:12:36 +0100

    init

---
+++ config/config.js
@@ -0,0 +1,6 @@
+// Copy this file as config.js in the same folder, with the proper database connection URI.
+
+module.exports = {
+  db: 'mongodb://shaunasbabyshower:ShaunasBabyShower@ds247499.mlab.com:47499/shaunas-babyshower',
+  db_dev: 'mongodb://localhost:27017/local',
+};
